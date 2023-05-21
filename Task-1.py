7ebab9dfb80bc88cf2fc070d26c5c7c912e3bb3c
krish-910 <77330476+krish-910@users.noreply.github.com>
2023-05-21 23:01:22 +0530

    Update Task-1.py

--- Task-1.py
+++ Task-1.py
@@ -1,25 +1,56 @@
-# Importing internet archive python library
 import internetarchive as ia
+import gzip
+import concurrent.futures
+import time
+import logging
 
-# Defining the collection name
-collection_name = 'nle-historicaltextbooks'
+# Configure logging
+logging.basicConfig(filename='error.log', level=logging.ERROR)
 
-# Defining the media type which we need to scan
+# Define the collection ID
+collection_id = 'usdanationalagriculturallibrary'
+
+# Define the media type
 media_type = 'texts'
 
-# Searching for all items in the collection with Media Type - Text
-items = ia.search_items('collection:' + collection_name + ' mediatype:' + media_type)
+# Retrieve all items in the collection with Media Type - Text
+items = list(ia.search_items('collection:' + collection_id + ' mediatype:' + media_type))
 
-# Loop through all the Full Text items and printing their URLs
-cnt = 0
-for item in items:
-    cnt += 1
-    print(str(cnt) + " " + item['identifier'])
-    
-    item_url = f"https://archive.org/details/{item_id}"
-    print("URL:", item_url)
+# Create a compressed file to store the URLs
+file_path = 'text_document_urls.txt.gz'
 
-    item_id = item['identifier']
-    item_metadata = ia.get_item(item_id)
-    print("Metadata:", item_metadata.metadata)
-    print()
+count = 0  # Counter for the number of URLs
+
+def process_items(chunk):
+    global count
+    for item in chunk:
+        item_id = item['identifier']
+        item_url = f"https://archive.org/details/{item_id}"
+        try:
+            with gzip.open(file_path, 'at', encoding='utf-8') as file:
+                count += 1
+                file.write(f"{count} {item_url}\n")
+        except Exception as e:
+            logging.error(f"Error writing URL for item {item_id}: {e}")
+
+# Measure the execution time
+start_time = time.time()
+
+# Set the number of threads
+num_threads = 6
+
+# Split the items into chunks
+chunk_size = (len(items)) // num_threads
+item_chunks = [items[i:i+chunk_size] for i in range(0, len(items), chunk_size)]
+
+# Use multi-threading to process the item chunks
+with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+    executor.map(process_items, item_chunks)
+
+end_time = time.time()
+execution_time = end_time - start_time
+
+print("Total URLs:", count)
+print("URLs saved to compressed file:", file_path)
+print("Execution time:", execution_time, "seconds")
+
