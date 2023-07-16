a5e47b817c8fba6391edf5490fec2b22a1a2ce58
krish-910 <77330476+krish-910@users.noreply.github.com>
2023-07-16 20:53:40 +0530

    Update Entity_Extraction.py

--- Task-2_nrc_docs_analysis/Entity_Extraction.py
+++ Task-2_nrc_docs_analysis/Entity_Extraction.py
@@ -16,6 +16,26 @@ def perform_ner(text):
         })
     return entities
 
+# Function to extract topics from the text
+def extract_topics(text):
+    topics = {
+        'environmental impact': 'TOPIC',
+        'economic assessment': 'TOPIC',
+        'safetywalkdown reports': 'TOPIC',
+        'radiation protection': 'TOPIC',
+        'fire protection': 'TOPIC',
+        'radioactive waste': 'TOPIC'
+        # Add more topics as needed
+    }
+    extracted_topics = []
+    for topic, entity_type in topics.items():
+        if topic in text:
+            extracted_topics.append({
+                'text': topic,
+                'entity_type': entity_type
+            })
+    return extracted_topics
+
 # Function to preprocess the input data and extract valuable information
 def preprocess_data(file_path, document_marker):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -26,7 +46,21 @@ def preprocess_data(file_path, document_marker):
                 start_new_doc = True
                 if current_doc:
                     entities = perform_ner(current_doc)
-                    yield entities
+                    topics = extract_topics(current_doc)
+                    entity_dict = {}
+                    for entity in entities:
+                        entity_type = entity['entity_type']
+                        if entity_type not in entity_dict:
+                            entity_dict[entity_type] = [entity['text']]
+                        else:
+                            entity_dict[entity_type].append(entity['text'])
+                    for topic in topics:
+                        entity_type = topic['entity_type']
+                        if entity_type not in entity_dict:
+                            entity_dict[entity_type] = [topic['text']]
+                        else:
+                            entity_dict[entity_type].append(topic['text'])
+                    yield entity_dict
                     current_doc = ""
             elif start_new_doc:
                 current_doc = line.strip() + " "
@@ -36,7 +70,21 @@ def preprocess_data(file_path, document_marker):
         # Process the last document if it exists
         if current_doc:
             entities = perform_ner(current_doc)
-            yield entities
+            topics = extract_topics(current_doc)
+            entity_dict = {}
+            for entity in entities:
+                entity_type = entity['entity_type']
+                if entity_type not in entity_dict:
+                    entity_dict[entity_type] = [entity['text']]
+                else:
+                    entity_dict[entity_type].append(entity['text'])
+            for topic in topics:
+                entity_type = topic['entity_type']
+                if entity_type not in entity_dict:
+                    entity_dict[entity_type] = [topic['text']]
+                else:
+                    entity_dict[entity_type].append(topic['text'])
+            yield entity_dict
 
 # Specify the input file path
 input_file = 'output.txt'
@@ -53,11 +101,11 @@ output_file = 'entities_output.csv'
 # Open the output file in write mode
 with open(output_file, 'w', encoding='utf-8', newline='') as output_file:
     writer = csv.writer(output_file)
-    writer.writerow(['Entity', 'Entity Type'])
+    writer.writerow(['Entity Type', 'Entities'])
 
     # Process the preprocessed data
     for entities in preprocessed_data:
-        for entity in entities:
-            writer.writerow([entity['text'], entity['entity_type']])
+        for entity_type, entity_list in entities.items():
+            writer.writerow([entity_type, entity_list])
 
 print("Entities output has been written to the file: entities_output.csv")
