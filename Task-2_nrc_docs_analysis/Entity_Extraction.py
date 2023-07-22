476aaf0e13dc7c0b6f5b54b94a182577cea87346
krish-910 <77330476+krish-910@users.noreply.github.com>
2023-07-22 22:24:41 +0530

    Update Entity_Extraction.py

--- Task-2_nrc_docs_analysis/Entity_Extraction.py
+++ Task-2_nrc_docs_analysis/Entity_Extraction.py
@@ -1,5 +1,4 @@
 import spacy
-import csv
 
 # Load the spaCy model
 nlp = spacy.load('en_core_web_sm')
@@ -11,21 +10,51 @@ entity_types_to_keep = ['PERSON', 'NORP', 'FAC', 'ORG', 'GPE', 'EVENT', 'DATE', 
 # Function to perform Named Entity Recognition (NER)
 def perform_ner(text):
     doc = nlp(text)
-    entities = set()  # Set to store unique entities
+    entities = {entity_type: [] for entity_type in entity_types_to_keep}
     for ent in doc.ents:
         if ent.label_ in entity_types_to_keep:
-            entities.add((ent.text, ent.label_))
+            entity_type = ent.label_
+            entity_text = ent.text
+            if entity_text not in entities[entity_type]:
+                entities[entity_type].append(entity_text)
     return entities
 
 # Function to extract topics from the text
 def extract_topics(text):
     topics = {
-        'environmental impact': 'TOPIC',
-        'economic assessment': 'TOPIC',
-        'safetywalkdown reports': 'TOPIC',
+        'reactor safety': 'TOPIC',
         'radiation protection': 'TOPIC',
-        'fire protection': 'TOPIC',
-        'radioactive waste': 'TOPIC'
+        'regulatory compliance': 'TOPIC',
+        'emergency preparedness': 'TOPIC',
+        'nuclear security': 'TOPIC',
+        'spent fuel management': 'TOPIC',
+        'public engagement and outreach': 'TOPIC',
+        'nuclear research and development': 'TOPIC',
+        'international cooperation': 'TOPIC',
+        'regulatory policy and rulemaking': 'TOPIC',
+        'nuclear waste disposal': 'TOPIC',
+        'nuclear accident investigations': 'TOPIC',
+        'nuclear plant inspections': 'TOPIC',
+        'nuclear safety culture': 'TOPIC',
+        'nuclear emergency exercises': 'TOPIC',
+        'nuclear incident reporting': 'TOPIC',
+        'nuclear power plant decommissioning': 'TOPIC',
+        'nuclear risk assessment': 'TOPIC',
+        'nuclear facility security assessments': 'TOPIC',
+        'nuclear materials transportation': 'TOPIC',
+        'nuclear waste storage': 'TOPIC',
+        'radiation monitoring': 'TOPIC',
+        'nuclear regulatory framework': 'TOPIC',
+        'nuclear licensing process': 'TOPIC',
+        'safety analysis and evaluation': 'TOPIC',
+        'nuclear emergency response plans': 'TOPIC',
+        'nuclear safeguards': 'TOPIC',
+        'nuclear energy policy': 'TOPIC',
+        'radiation dose limits': 'TOPIC',
+        'nuclear fuel cycle': 'TOPIC',
+        'nuclear facility design': 'TOPIC',
+        'nuclear workforce training': 'TOPIC',
+        'nuclear security exercises': 'TOPIC'
         # Add more topics as needed
     }
     extracted_topics = []
@@ -48,20 +77,10 @@ def preprocess_data(file_path, document_marker):
                 if current_doc:
                     entities = perform_ner(current_doc)
                     topics = extract_topics(current_doc)
-                    entity_dict = {}
-                    for entity, entity_type in entities:
-                        if entity_type not in entity_dict:
-                            entity_dict[entity_type] = set([entity])
-                        else:
-                            entity_dict[entity_type].add(entity)
-                    for topic in topics:
-                        entity_type = topic['entity_type']
-                        entity = topic['text']
-                        if entity_type not in entity_dict:
-                            entity_dict[entity_type] = set([entity])
-                        else:
-                            entity_dict[entity_type].add(entity)
-                    yield entity_dict
+                    # Remove duplicates from entities
+                    for entity_type, entity_list in entities.items():
+                        entities[entity_type] = list(set(entity_list))
+                    yield {'entities': entities, 'topics': topics}
                     current_doc = ""
             elif start_new_doc:
                 current_doc = line.strip() + " "
@@ -72,20 +91,10 @@ def preprocess_data(file_path, document_marker):
         if current_doc:
             entities = perform_ner(current_doc)
             topics = extract_topics(current_doc)
-            entity_dict = {}
-            for entity, entity_type in entities:
-                if entity_type not in entity_dict:
-                    entity_dict[entity_type] = set([entity])
-                else:
-                    entity_dict[entity_type].add(entity)
-            for topic in topics:
-                entity_type = topic['entity_type']
-                entity = topic['text']
-                if entity_type not in entity_dict:
-                    entity_dict[entity_type] = set([entity])
-                else:
-                    entity_dict[entity_type].add(entity)
-            yield entity_dict
+            # Remove duplicates from entities
+            for entity_type, entity_list in entities.items():
+                entities[entity_type] = list(set(entity_list))
+            yield {'entities': entities, 'topics': topics}
 
 # Specify the input file path
 input_file = 'output.txt'
@@ -97,16 +106,19 @@ document_marker = '<DOCUMENT_END_MARKER>'
 preprocessed_data = preprocess_data(input_file, document_marker)
 
 # Specify the output file path
-output_file = 'entities_output.csv'
+output_file = 'entities_output.txt'
 
 # Open the output file in write mode
-with open(output_file, 'w', encoding='utf-8', newline='') as output_file:
-    writer = csv.writer(output_file)
-    writer.writerow(['Entity Type', 'Entities'])
-
+with open(output_file, 'w', encoding='utf-8') as output_file:
     # Process the preprocessed data
-    for entities in preprocessed_data:
-        for entity_type, entity_set in entities.items():
-            writer.writerow([entity_type, ', '.join(entity_set)])
+    for doc in preprocessed_data:
+        entities = doc['entities']
+        entity_text = ", ".join(
+            "'{}: [{}]'".format(entity_type, ", ".join(entity_list)) for entity_type, entity_list in entities.items()
+        )
+        topic_text = ", ".join(
+            "'{}: [{}]'".format(topic['entity_type'], topic['text']) for topic in doc['topics']
+        )
+        output_file.write("\"(%s, %s),\"" % (entity_text, topic_text))
 
-print("Entities output has been written to the file: entities_output.csv")
+print("Entities output has been written to the file: entities_output.txt")
