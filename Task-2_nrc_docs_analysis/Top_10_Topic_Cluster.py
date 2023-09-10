aaf01d493f7f5beae8e8b1296a9d31428ae963d8
krish-910 <77330476+krish-910@users.noreply.github.com>
2023-09-10 17:24:49 +0530

    Update Top_10_Topic_Cluster.py

--- Task-2_nrc_docs_analysis/Top_10_Topic_Cluster.py
+++ Task-2_nrc_docs_analysis/Top_10_Topic_Cluster.py
@@ -231,10 +231,10 @@ try:
           if doc_index < len(Document_Titles):
             doc_title = Document_Titles[doc_index]
             pdf_url = "http://web.archive.org/web/" + Document_Titles[doc_index]
+            doc_topics = doc['topics']
             # Check if any of the top 10 topics are present in the document
             if any(topic in top_10_topics for topic in doc_topics):
-                data_row = f'{pdf_url}>{doc_title}\t'
-            doc_topics = doc['topics']
+                data_row = f'{pdf_url}>{doc_title}\t'            
             for topic in top_10_topics:
                 topic_present = topic in doc_topics
                 data_row += "1\t" if topic_present else "0\t"
